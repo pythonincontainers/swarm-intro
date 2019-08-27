67a83449b68433c496834a66e08c6799604c5499
pythonincontainers <krzysztof_celmer@hotmail.com>
2019-08-27 13:02:26 +0200

    Example files

---
+++ flask-hostname/flask-hostname_v1.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+app = Flask(__name__)
+
+import socket
+hostname = socket.gethostname()
+
+@app.route('/')
+def hello_world():
+    return 'Flask Hello world! I am running on '+hostname
+
+@app.route('/test')
+def test():
+    message = 'Testing hidden functionality ;) \n'
+    message += 'request.host='+request.host+'\n'
+    message += 'request.remote_addr='+request.remote_addr+'\n'
+    try:
+        secret_file = open("/run/secrets/my_password", "r")
+    except:
+        pass
+    else:
+        message += 'my_password is '+secret_file.read()+'\n'        
+    return message
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0')
